0f7609d991d35b93ad8feb0ab99c0e133f0f6592
isadorw <isadoraitodacunha12@gmail.com>
2024-04-28 17:27:55 -0400

    cypress day 2 - section 3, 4 and 5

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -37,7 +37,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#lastName').type('Ito')
         cy.get('#email').type('isadoraito@gmail.com')
         cy.get('#open-text-area').type(longText, {delay: 0}) // delay: 0 ajuda o teste a ser mais rápido
-        cy.get('button[type="submit"]').click()
+        cy.contains('button','Enviar').click()
 
         cy.get('.success').should('be.visible')
     })
@@ -46,8 +46,8 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#firstName').type('Isadora')
         cy.get('#lastName').type('Ito')
         cy.get('#email').type('isadoraito@gmail,com')
-        cy.get('#open-text-area').type('Quero reemboloso') // delay: 0 ajuda o teste a ser mais rápido
-        cy.get('button[type="submit"]').click()
+        cy.get('#open-text-area').type('Quero reemboloso')
+        cy.contains('button','Enviar').click()
 
         cy.get('.error').should('be.visible')
     })
@@ -56,5 +56,79 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#phone')
             .type('abcdefghij')
             .should('have.value', '')
+    })
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+        cy.get('#firstName').type('Isadora')
+        cy.get('#lastName').type('Ito')
+        cy.get('#email').type('isadoraito@gmail.com')
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('Quero reemboloso')
+        cy.contains('button','Enviar').click()
+        
+        cy.get('.error').should('be.visible')
+    }) 
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
+        cy.get('#firstName')
+            .type('Isadora')
+            .should('have.value', 'Isadora')
+            .clear().should('have.value', '')
+        cy.get('#lastName')
+            .type('Ito')
+            .should('have.value', 'Ito')
+            .clear().should('have.value', '')
+        cy.get('#email')
+            .type('isadoraito@gmail.com')
+            .should('have.value', 'isadoraito@gmail.com')
+            .clear().should('have.value', '')
+        cy.get('#phone')
+            .type('65999999999')
+            .should('have.value', '65999999999')
+            .clear().should('have.value', '')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function() {
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+    it('envia o formuário com sucesso usando um comando customizado', function() {
+        cy.fillMandatoryFieldsAndSubmit()
+
+        cy.get('.success').should('be.visible')
+    })
+
+    it('seleciona um produto (YouTube) por seu texto', function() {
+        cy.get('#product')
+            .select('YouTube')
+            .should('have.value', 'youtube')
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', function() {
+        cy.get('#product')
+            .select('mentoria')
+            .should('have.value', 'mentoria')
+    })
+    it('seleciona um produto (Blog) por seu índice', function() {
+        cy.get('#product')
+            .select(1)
+            .should('have.value', 'blog')
+    })
+
+    it('marca o tipo de atendimento "Feedback"', function() {
+        cy.get('input[type="radio"][value="feedback"]')
+            .check()
+            .should('have.value', 'feedback')
+    })
+
+    it.only('marca cada tipo de atendimento', function() {
+        cy.get('input[type="radio"]')
+            .should('have.length', 3)
+            .each(function($radio) {
+                cy.wrap($radio).check()
+                cy.wrap($radio).should('be.checked')
+            })
     })
   })
